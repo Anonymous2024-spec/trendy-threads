d2266c006e62e2d90f0437228c24701f72fed529
Naana Shifah <nanashifah2@gmail.com>
2025-04-30 17:29:46 +0300

    Enhance: Redesign and refactor entire dashboard skeleton with Ant Design layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
+import AppLayout from "../Layout";
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <div className="flex h-screen">
-      <Sidebar />
-      
-      <div className="flex flex-col flex-1">
-        <Header />
-        
-        <main className="flex-1 overflow-auto">
-        </main>
-        
-        <Footer />
-      </div>
-    </div>
-  );
+  return <AppLayout />;
 }
 
 export default App;
